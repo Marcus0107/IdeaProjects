40a196c75360b5cfeeca2c3cfc1a3951159d4db8
Marcus0107 <danzer.marcus@gmail.com>
2017-05-11 21:08:34 +0200

    Added stuff from Johennes Schneider

--- ObjektOrientierung/src/Musterlösung/AbstractFactory/InvoiceHeaderFactory.java
+++
@@ -1,7 +0,0 @@
-package Musterlösung.AbstractFactory;
-
-/**
- * Created by Marcus on 07.04.2017.
- */public interface InvoiceHeaderFactory {
-    InvoiceHeader createHeader( Receiver receiver );
-}
